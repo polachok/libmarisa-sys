12b7eec5e9c335e4f3fda77666e9c49de1f861ff
Alexander Polakov <apolyakov@beget.ru>
2015-05-28 13:02:00 +0300

    Fix signature in previous

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 extern crate libc;
-use libc::{c_char, c_int};
+use libc::{c_char, c_int, size_t};
 
 #[repr(C)] pub struct KeySet;
 #[repr(C)] pub struct Trie;
@@ -9,7 +9,7 @@ use libc::{c_char, c_int};
 extern "C" {
     pub fn keyset_create() -> *const KeySet;
     pub fn keyset_push(keyset: *const KeySet, string: *const c_char, len: c_int, val: c_int);
-    pub fn keyset_num_keys(keyset: *const KeySet);
+    pub fn keyset_num_keys(keyset: *const KeySet) -> size_t;
     pub fn keyset_destroy(keyset: *const KeySet);
     pub fn trie_create() -> *const Trie;
     pub fn trie_build(trie: *const Trie, keyset: *const KeySet, options: c_int);
